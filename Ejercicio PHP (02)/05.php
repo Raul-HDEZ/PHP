03cd536a812e9fdc9892671106946ae3dd8b08cd
Alumo (A) <raul.hdez.2001@outlook.es>
2021-09-24 12:31:24 +0200

    24-Sep-2021

---
+++ Ejercicio PHP (02)/05.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 5</title>
+    <?php
+    function generarHTMLTable ( $filas, $columnas, $borde,$contenido){
+        echo "<tr>";
+        // <tr> para las filas
+        // <td> para las columnas
+        // Crear un for que cree filas y dentro un for que haga columnas
+        echo "</tr>";
+    }
+    ?>
+</head>
+<body>
+    <h3>5. Realizar y probar una  función que genere el código HTML de tablas con un borde determinado, incluyendo en cada casilla el mismo texto. </h3>
+</body>
+</html>
