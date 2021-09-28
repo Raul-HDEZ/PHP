5d1fe06907fdc76facbeb6601582ac74f95b77b9
raul-hdez <raul.hdez.2001@outlook.es>
2021-09-28 14:02:47 +0200

    28-Sep-2021

--- Ejercicio PHP(03)/02.php
+++ Ejercicio PHP(03)/02.php
@@ -7,6 +7,12 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+   // 2.- Crear un array que almacene 5 cadenas con el nombre de periódicos 
+   // y sus enlaces para acceder. El array será asociativo con el nombre del 
+   // periódico como clave y su URL como valor.
+
+   $medios =  [ "El Pais" => "https://www.elpais.com", "El Mundo" => "https://www.elmundo.es", "ABC" => "",
+    ?>
 </body>
 </html>
