a38fc2bf50ed40e0f9fa049568952ba84044a14f
Raul Hernandez <mjolnirk@localhost.localdomain>
2021-10-05 19:46:31 +0200

    5-Oct-2021

--- Ejercicio PHP(04)/01.php
+++ Ejercicio PHP(04)/01.php
@@ -1,18 +1,33 @@
 <code>
     <?php 
     $contraseñas = ["admin" => "admin", "user01" => "user01", "user02" => "user02"];
-    // Meterlo en una funcion y llamarla al cargar
-    foreach ($contraseñas as $key => $value) {
-        if ($_POST['user'] == $key) {
-            if ($_POST['passwd' == $value]) {
-                return true;
-            }else {
-                return false;
+
+    function Login($log){
+        foreach ($contraseñas as $key => $value) {
+            if (strcmp($log['user'], $key) == 0 && strcmp($log['passwd'], $value)  == 0) {
+                return true;   
             }
-        }else {
-            return false;
         }
+        return false;
     }
     
+    function Bienvenida() {
+        echo "Bienvenido usuario: " .$_POST['user'];
+        exit;
+    }
+
+    function Error(){
+        echo "usuario o contraseña invalidos <br>";
+        echo "<input type='button' value='Volver a intentarlo' onClick='history.go(-1);'>";
+
+    }
+
+    foreach ($contraseñas as $key => $value) {
+        if (strcmp($_POST['user'], $key) == 0 && strcmp($_POST['passwd'], $value)  == 0) {
+            Bienvenida();   
+        }
+    }
+    Error();
+
     ?>
 </code>
