dbe945d73c9607db2a334ec813c16c21da813166
raul-hdez <raul.hdez.2001@outlook.es>
2021-10-05 14:01:19 +0200

    5-Oct-2021

--- Ejercicio PHP(04)/01.php
+++ Ejercicio PHP(04)/01.php
@@ -1,10 +1,16 @@
 <code>
     <?php 
     $contraseñas = ["admin" => "admin", "user01" => "user01", "user02" => "user02"];
-
+    // Meterlo en una funcion y llamarla al cargar
     foreach ($contraseñas as $key => $value) {
         if ($_POST['user'] == $key) {
-            # code...
+            if ($_POST['passwd' == $value]) {
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return false;
         }
     }
     
