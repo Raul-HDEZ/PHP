e9e96b7b74fadcb2cbca5cb08b7291f4457c5706
Raul Hernandez <mjolnirk@localhost.localdomain>
2021-10-06 10:36:37 +0200

    6-Oct-2021

--- Ejercicio PHP(04)/04.php
+++ Ejercicio PHP(04)/04.php
@@ -1,5 +1,32 @@
 <code>
     <?php 
+    if (empty($_POST['publi'])) {
+        $_POST['publi']= "No";
+    }
+    $idiomas = $_POST['idioma'];
+    echo <<<EOS
+    Nombre : $_POST[nombre] <br>
+    Clave : $_POST[contraseña] <br>
+    Semaforo $_POST[semaforo] <br>
+    Publicidad : $_POST[publi] publicidad <br>
+    Idiomas : 
+    EOS;
+    foreach ($idiomas as $value) {
+       echo $value . ", ";
+    }
+    echo "<br>";
     
+    echo <<< EEP
+    Año de fin de estudios : $_POST[ano] <br>
+    Lista de los codigos postales de provincias :
+    EEP;
+    $postal = $_POST['ciudades'];
+    foreach ($postal as $value) {
+        echo $value . ", ";
+    }
+    echo<<<EEO
+    <br>
+    Comentarios : $_POST[textarea]
+    EEO;
     ?>
 </code>
