fba35c1aa407d7e01be6e12536c65cfac5f09d1c
raul-hdez <raul.hdez.2001@outlook.es>
2021-10-15 12:47:42 +0200

    15-Oct-2021

--- Envio de Ficheros/procesarficheros.php
+++ Envio de Ficheros/procesarficheros.php
@@ -23,15 +23,94 @@
             if ($_SERVER['REQUEST_METHOD'] == "GET") {
                 include_once "./Formulario.html";
             } else {
-                $mensaje = '';// No se recibe nada, error al enviar el POST, se supera post_max_size
+                $mensaje = '';
             if (count($_POST) == 0 ){
                    $mensaje= "  Error: se supera el tamaño máximo de un petición POST ";   
                 }
             }
 
+            function checkSize($array){
+                $suma = 0;
+                for ($i=0; $i < count($array['archivos']['size']); $i++) { 
+                    $suma += $array['archivos']['size'][$i];
+                }
+                if ($suma > 300000) {
+                    return false;
+                }
+                return true;
+            }
+
+            function checkImage($tipo){
+                if ($tipo == "image/jpeg" || $tipo == "image/png") {
+                    return true;               
+                }
+                return false;
+            }
+
+            function checkEnvio($array){
+                if ($array['archivos']['size'][0] != 0 ) {
+                    return true;                  
+                }
+                return false;
+            }
             
-            
-        // "image/jpg"  "image/png" para la estension
+            function checkFile($nombre){
+                $path = "/home/alumnoa/imgusers/".$nombre;
+                if (file_exists($path)) {
+                    return true;                  
+                }
+                return false;
+            }
+
+            function checkAll($array, $errorarray){
+                $error = "";
+                $añadir = (checkSize($array)) ? "" : "Los archivos superan el limete de tamaño <br>";
+                $error = $error . $añadir;
+                $añadir = "";
+                if (checkEnvio($array)) {
+                    for ($i=0; $i < count($array['archivos']['name']); $i++) { 
+                        $añadir = (checkImage($array['archivos']['type'][$i])) ? "" : "El archivo ". $array['archivos']['name'][$i] ." no es una imagen <br>" ;
+                        $error = $error . $añadir;
+                        $añadir = "";
+                        $añadir = (checkFile($array['archivos']['name'][$i])) ? "El archivo ". $array['archivos']['name'][$i] ." ya existe en el servidor <br>" : "" ;
+                        $error = $error . $añadir;
+                        $añadir = "";
+                        $añadir = ($array['archivos']['error'][$i]>0) ? $errorarray[$array['archivos']['error'][$i]]."<br>" : "" ;
+                        $error = $error . $añadir;
+                        $añadir = "";
+                    }
+                } else {
+                    $error = "No se ha enviado nada";
+                }
+                return $error;
+            }
+
+            function checkSave($array,$elemento){
+                $msg = "";
+                if (checkImage($array['archivos']['type'][$elemento]) == true && checkFile($array['archivos']['name'][$elemento]) == false  && checkSize($array)) {
+                    $msg = "El archivo ". $array['archivos']['name'][$elemento] . " se ha subido al servidor <br>";
+                }
+                return $msg;
+            }
+            function checkSaveboolean($array,$elemento){
+                if (checkImage($array['archivos']['type'][$elemento]) == true && checkFile($array['archivos']['name'][$elemento]) == false && checkSize($array)) {
+                    return true;
+                }
+                return false;
+            }
+
+            function saveFile($array,$elemento){
+                move_uploaded_file($array['archivos']['tmp_name'][$elemento], "/home/alumnoa/imgusers/". $array['archivos']['name'][$elemento] );
+            }
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {        
+                print(checkAll($_FILES, $codigosErrorSubida));       
+                for ($i=0; $i < count($_FILES['archivos']['name']); $i++) { 
+                    print(checkSave($_FILES,$i));
+                    if (checkSaveboolean($_FILES,$i)) {
+                        saveFile($_FILES,$i);
+                    }
+                }
+            }
     ?>
 </body>
 </html>
