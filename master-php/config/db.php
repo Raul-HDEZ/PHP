a6599427b8ae459693df1d52078acb6df1061af1
Julia <julia.joven@gmail.com>
2022-01-13 13:58:23 +0100

    13-Jun-2022

--- master-php/config/db.php
+++ master-php/config/db.php
@@ -2,11 +2,7 @@
 
 class Database{
 	public static function connect(){
-<<<<<<< HEAD
 		$db = new mysqli('localhost', 'raul', 'raul', 'camisetas_master');
-=======
-		$db = new mysqli('localhost', 'raul', 'raul', 'tienda_master');
->>>>>>> 4008e9ce0bf98a53357ad8b43241f94f6d79dcb0
 		$db->query("SET NAMES 'utf8'");
 		return $db;
 	}
