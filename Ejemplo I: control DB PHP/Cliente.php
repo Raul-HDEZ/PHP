4008e9ce0bf98a53357ad8b43241f94f6d79dcb0
raul-hdez <raul.hdez.2001@outlook.es>
2022-01-13 08:46:12 +0100

    13-Jun-2022

--- Ejemplo I: control DB PHP/Cliente.php
+++ Ejemplo I: control DB PHP/Cliente.php
@@ -1,9 +1,25 @@
-<?php
 
-class Cliente
-{
-    public $cod_cliente;
-    public $nombre;
-    public $clave;
-    public $veces;
+<?php 
+class Cliente {
+  private $cod_cliente;
+  private $nombre;
+  private $clave;
+  private $veces;
+  
+  // Métodos mágicos para setter y getter
+  
+  public function __set($nombre,$valor){
+      $class = get_class($this);
+      if ( property_exists($class, $nombre)){
+          $this->$nombre = $valor; // Ojo $nombre
+      }       
+  }
+  
+  public function __get($nombre){
+      $class = get_class($this);
+      if ( property_exists($class, $nombre)){
+          return  $this->$nombre;
+      }
+   }
 }
+?>
--- Ejemplo I: control DB PHP/Pedido.php
+++ Ejemplo I: control DB PHP/Pedido.php
@@ -1,9 +1,24 @@
-<?php
-
-class Pedido
-{
-    public $numped;
-    public $cod_cliente;
-    public $producto;
-    public  $precio;
+<?php 
+class Pedido {
+   private $numped;
+   private $cod_cliente;
+   private $producto;
+   private $precio;
+   
+   // Métodos mágicos para setter y getter
+   
+   public function __set($nombre,$valor){
+       $class = get_class($this);
+       if ( property_exists($class, $nombre)){
+           $this->$nombre = $valor; // Ojo $nombre
+       }       
+   }
+   
+   public function __get($nombre){
+       $class = get_class($this);
+       if ( property_exists($class, $nombre)){
+           return  $this->$nombre;
+       }
+   }
+   
 }
