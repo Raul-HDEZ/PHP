b13f5535b0e11aec6fef40b085d28ba27e94bf66
raul-hdez <raul.hdez.2001@outlook.es>
2022-01-14 10:52:38 +0100

    14-Jun-2022

--- master-php/config/parameters.php
+++ master-php/config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url", "http://localhost/julia/master-php/");
+define("base_url", "http://localhost/alumnoa/master-php/");
 define("controller_default", "productoController");
 define("action_default", "index");
 
